b2d57e9116e6f589ea828b335717e14b7ec98d64
Mehdi <mahdykamali354@yahoo.com>
2026-02-11 16:08:09 +0330

    [feature] base entities added

---
+++ src/main/java/com/gramsense/persistence/entity/UserStats.java
@@ -0,0 +1,50 @@
+package com.gramsense.persistence.entity;
+
+import com.gramsense.persistence.entity.enumeration.GrammarLevel;
+import com.gramsense.persistence.entity.enumeration.GrammarTopic;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Mehdi Kamali
+ * @since 11/02/2026
+ */
+
+@Entity
+@Table(name = "user_stats")
+public class UserStats {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "grammar_user_id", nullable = false)
+    private GrammarUser grammarUser;
+
+    @Column(name = "grammar_topic", nullable = false, length = 50)
+    private GrammarTopic grammarTopic;
+
+    @Column(name = "grammar_level", nullable = false, length = 25)
+    private GrammarLevel grammarLevel;
+
+    @Column(name = "total_sent_evaluations")
+    private int totalSentEvaluations;
+
+    @Column(name = "total_correct_evaluations")
+    private int totalCorrectEvaluations;
+
+    @Column(name = "total_seen_examples")
+    private int totalSeenExamples;
+
+    @Column(name = "total_attempted_exercises")
+    private int totalAttemptedExercises;
+
+    @Column(name = "total_correct_exercises")
+    private int totalCorrectExercises;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
