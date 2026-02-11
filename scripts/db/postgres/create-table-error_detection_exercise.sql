c69f6b8c671c2fe80a14d90065787404fa978366
Mehdi <mahdykamali354@yahoo.com>
2026-02-11 21:30:53 +0330

    [enhancement] dev docs and sample added

--- scripts/db/postgres/create-table-error_detection_exercise.sql
+++
@@ -1,7 +0,0 @@
-create table ERROR_DETECTION_EXERCISE
-(
-    GRAMMAR_EXERCISE_ID bigint primary key,
-    QUESTION            text,
-    ANSWER              text,
-    constraint FK_ERROR_DETECTION_EXERCISE foreign key (GRAMMAR_EXERCISE_ID) references GRAMMAR_EXERCISE (GRAMMAR_EXERCISE_ID)
-);
--- scripts/db/postgres/create-table-full_sentence_exercise.sql
+++
@@ -1,7 +0,0 @@
-create table FULL_SENTENCE_EXERCISE
-(
-    GRAMMAR_EXERCISE_ID bigint primary key,
-    OPTIONS             text,
-    ANSWER              text,
-    constraint FK_FULL_SENTENCE_EXERCISE foreign key (GRAMMAR_EXERCISE_ID) references GRAMMAR_EXERCISE (GRAMMAR_EXERCISE_ID)
-);
